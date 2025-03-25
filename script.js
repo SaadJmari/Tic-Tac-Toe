8d29625d98e0d826214019f9b469d90b1f61decd
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-03-25 17:17:08 +0000

    Add win and draw conditions

--- script.js
+++ script.js
@@ -36,8 +36,8 @@ function Gameboard() {
 
 /*
 ** Cell == square in the board
-** cell's state: 
-** 0: empty
+** Cell's state: 
+** 0: Empty
 ** 1: Player's one token
 ** 2: Player's two token
 */
@@ -88,7 +88,68 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
 
     const playRound = (row, column) => {
         const moveSuccessful = board.makeMove(row, column, getActivePlayer().token)
+        const currentBoard = board.getBoard()
+        const player = getActivePlayer()
+        let fullCellCounter = 0
+
         if (moveSuccessful) {
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 3; j++) {
+                    if (currentBoard[i][j].getValue() !== 0) { fullCellCounter++ }
+                }
+            }
+            if (row === column || row + column === 2) {
+                if (currentBoard[0][0].getValue() === player.token
+                    && currentBoard[1][1].getValue() === player.token
+                    && currentBoard[2][2].getValue() === player.token) {
+                    board.printBoard()
+                    console.log(`${player.name} is the winner`)
+                    return true
+                }
+                else if (currentBoard[0][2].getValue() === player.token
+                    && currentBoard[1][1].getValue() === player.token
+                    && currentBoard[2][0].getValue() === player.token) {
+                    board.printBoard()
+                    console.log(`${player.name} is the winner`)
+                    return true
+                }
+                else if (currentBoard[row][0].getValue() === player.token
+                    && currentBoard[row][1].getValue() === player.token
+                    && currentBoard[row][2].getValue() === player.token) {
+                    board.printBoard()
+                    console.log(`${player.name} is the winner`)
+                    return true
+                } else if (currentBoard[0][column].getValue() === player.token
+                    && currentBoard[1][column].getValue() === player.token
+                    && currentBoard[2][column].getValue() === player.token) {
+                    board.printBoard()
+                    console.log(`${player.name} is the winner`)
+                    return true
+                }
+                else if (fullCellCounter === 9) {
+                    board.printBoard()
+                    console.log("It's a draw!!")
+                    return false
+                }
+            } else if (currentBoard[row][0].getValue() === player.token
+                && currentBoard[row][1].getValue() === player.token
+                && currentBoard[row][2].getValue() === player.token) {
+
+                board.printBoard()
+                console.log(`${player.name} is the winner`)
+                return true
+            } else if (currentBoard[0][column].getValue() === player.token
+                && currentBoard[1][column].getValue() === player.token
+                && currentBoard[2][column].getValue() === player.token) {
+                board.printBoard()
+                console.log(`${player.name} is the winner`)
+                return true
+            }
+            else if (fullCellCounter === 9) {
+                board.printBoard()
+                console.log("It's a draw!!")
+                return false
+            }
             switchPlayerTurn()
             printNewRound()
         }
@@ -101,3 +162,54 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
 }
 
 const game = gameController()
+
+
+//This function is for testing edge cases
+// function runTestCases() {
+//     console.log("==== TEST 1: Player wins on top row ====");
+//     let game1 = gameController();
+//     game1.playRound(0, 0);
+//     game1.playRound(1, 0);
+//     game1.playRound(0, 1);
+//     game1.playRound(1, 1); 
+//     game1.playRound(0, 2); 
+
+//     console.log("\n==== TEST 2: Player wins on diagonal ====");
+//     let game2 = gameController();
+//     game2.playRound(0, 0);
+//     game2.playRound(0, 1);
+//     game2.playRound(1, 1);
+//     game2.playRound(0, 2);
+//     game2.playRound(2, 2);
+
+//     console.log("\n==== TEST 3: Draw condition ====");
+//     let game3 = gameController();
+//     game3.playRound(0, 0);
+//     game3.playRound(0, 1);
+//     game3.playRound(0, 2);
+//     game3.playRound(1, 1);
+//     game3.playRound(1, 0);
+//     game3.playRound(1, 2);
+//     game3.playRound(2, 1);
+//     game3.playRound(2, 0);
+//     game3.playRound(2, 2);
+
+//     console.log("\n==== TEST 4: Invalid move (occupied cell) ====");
+//     let game4 = gameController();
+//     game4.playRound(0, 0);
+//     game4.playRound(0, 0);
+
+//     console.log("\n==== TEST 5: Win on last move ====");
+//     let game5 = gameController();
+//     game5.playRound(0, 0);
+//     game5.playRound(0, 1); 
+//     game5.playRound(0, 2); 
+//     game5.playRound(1, 1); 
+//     game5.playRound(1, 0); 
+//     game5.playRound(1, 2);
+//     game5.playRound(2, 1); 
+//     game5.playRound(2, 0);
+//     game5.playRound(2, 2); 
+// }
+
+// runTestCases();
