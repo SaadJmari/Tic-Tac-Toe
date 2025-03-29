d2b60b59cbe4f446faf219c2fc8a6c558488fe2e
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-03-29 18:22:50 +0000

    Add styling to the page, and fixed some coding errors

--- script.js
+++ script.js
@@ -60,6 +60,8 @@ function Cell() {
 
 function gameController(playerOne = "Player One", playerTwo = "Player Two") {
     const board = Gameboard()
+    let gameOver = false
+    let winningCells = []
 
     const players = [
         {
@@ -81,6 +83,10 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
 
     const getActivePlayer = () => activePlayer
 
+    const isGameOver = () => gameOver
+
+    const getWinningCells = () => winningCells
+
     const printNewRound = () => {
         board.printBoard()
         console.log(`${getActivePlayer().name}'s turn`)
@@ -91,6 +97,8 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
         const currentBoard = board.getBoard()
         const player = getActivePlayer()
         let fullCellCounter = 0
+
+
 
         if (moveSuccessful) {
             for (let i = 0; i < 3; i++) {
@@ -104,6 +112,8 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
                     && currentBoard[2][2].getValue() === player.token) {
                     board.printBoard()
                     console.log(`${player.name} is the winner`)
+                    gameOver = true
+                    winningCells = [[0, 0], [1, 1], [2, 2]]
                     return true
                 }
                 else if (currentBoard[0][2].getValue() === player.token
@@ -111,6 +121,8 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
                     && currentBoard[2][0].getValue() === player.token) {
                     board.printBoard()
                     console.log(`${player.name} is the winner`)
+                    gameOver = true
+                    winningCells = [[0, 2], [1, 1], [2, 0]]
                     return true
                 }
                 else if (currentBoard[row][0].getValue() === player.token
@@ -118,17 +130,22 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
                     && currentBoard[row][2].getValue() === player.token) {
                     board.printBoard()
                     console.log(`${player.name} is the winner`)
+                    gameOver = true
+                    winningCells = [[row, 0], [row, 1], [row, 2]]
                     return true
                 } else if (currentBoard[0][column].getValue() === player.token
                     && currentBoard[1][column].getValue() === player.token
                     && currentBoard[2][column].getValue() === player.token) {
                     board.printBoard()
                     console.log(`${player.name} is the winner`)
+                    gameOver = true
+                    winningCells = [[0, column], [1, column], [2, column]]
                     return true
                 }
                 else if (fullCellCounter === 9) {
                     board.printBoard()
                     console.log("It's a draw!!")
+                    gameOver = true
                     return false
                 }
             } else if (currentBoard[row][0].getValue() === player.token
@@ -137,17 +154,22 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
 
                 board.printBoard()
                 console.log(`${player.name} is the winner`)
+                gameOver = true
+                winningCells = [[row, 0], [row, 1], [row, 2]]
                 return true
             } else if (currentBoard[0][column].getValue() === player.token
                 && currentBoard[1][column].getValue() === player.token
                 && currentBoard[2][column].getValue() === player.token) {
                 board.printBoard()
                 console.log(`${player.name} is the winner`)
+                gameOver = true
+                winningCells = [[0, column], [1, column], [2, column]]
                 return true
             }
             else if (fullCellCounter === 9) {
                 board.printBoard()
                 console.log("It's a draw!!")
+                gameOver = true
                 return false
             }
             switchPlayerTurn()
@@ -158,10 +180,96 @@ function gameController(playerOne = "Player One", playerTwo = "Player Two") {
         }
     }
 
-    return { playRound, getActivePlayer }
+    return { playRound, getActivePlayer, getBoard: board.getBoard, isGameOver, getWinningCells }
 }
 
-const game = gameController()
+
+function ScreenController() {
+    const pOneName = document.querySelector("#playerOne")
+    const pTwoName = document.querySelector("#playerTwo")
+    const turnDiv = document.querySelector(".turn")
+    const boardDiv = document.querySelector(".board")
+    const startBtn = document.querySelector(".startBtn")
+    const resetBtn = document.querySelector(".resetBtn")
+
+    let game
+    let p1 = "Player One"
+    let p2 = "Player Two"
+
+
+    function startGame() {
+        p1 = pOneName.value || "Player One"
+        p2 = pTwoName.value || "Player Two"
+        game = gameController(p1, p2)
+        updateScreen()
+    }
+
+    function resetGame() {
+        if (!game) return
+        game = gameController(p1, p2)
+        updateScreen()
+    }
+
+
+    const updateScreen = () => {
+
+        const winningCells = game.getWinningCells()
+
+        if (!game) return
+        //Clear board
+        boardDiv.textContent = ""
+
+        //Get the newest version of the board and player turn
+        const board = game.getBoard()
+        const activePlayer = game.getActivePlayer()
+
+        //Render player's turn
+        turnDiv.textContent = game.isGameOver() ? `Game Over` : `${activePlayer.name}'s turn..`
+
+        //Render the board
+        board.forEach((row, rowIndex) => {
+            row.forEach((cell, columnIndex) => {
+                const cellButton = document.createElement("button")
+                cellButton.classList.add("cell")
+                cellButton.dataset.row = rowIndex
+                cellButton.dataset.column = columnIndex
+                cellButton.textContent = cell.getValue() === 0 ? "" : cell.getValue() === 1 ? "X" : "O"
+                if (cell.getValue() === 1) {
+                    cellButton.classList.add("playerX")
+                } else if (cell.getValue() === 2) {
+                    cellButton.classList.add("playerO")
+                }
+                const isWinningCell = winningCells.some(([r, c]) => r === rowIndex && c === columnIndex)
+                if (isWinningCell) {
+                    cellButton.classList.add("win-cell")
+                }
+                boardDiv.appendChild(cellButton)
+            })
+        })
+    }
+
+    function clickHandlerBoard(e) {
+
+        if (!game || game.isGameOver()) return
+
+        const selectedRow = e.target.dataset.row
+        const selectedColumn = e.target.dataset.column
+
+
+        if (!selectedRow || !selectedColumn) return
+
+        game.playRound(Number(selectedRow), Number(selectedColumn))
+        updateScreen()
+
+    }
+
+    startBtn.addEventListener("click", startGame)
+    resetBtn.addEventListener("click", resetGame)
+    boardDiv.addEventListener("click", clickHandlerBoard)
+
+}
+
+ScreenController()
 
 
 //This function is for testing edge cases
